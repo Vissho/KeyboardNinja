ef8715479a74761f74d94ab8d455249b2ca02dab
Ilya Lisovitskyi <Vissho@users.noreply.github.com>
2022-04-28 22:20:01 +0700

    Made string analysis functions

--- src/libkeyboardninja/kbn_read.h
+++ src/libkeyboardninja/kbn_read.h
@@ -1,5 +1,5 @@
-#ifndef _KEYBOARDNINJA_
-#define _KEYBOARDNINJA_
+#ifndef _KBN_READ_
+#define _KBN_READ_
 
 #define SIZE_STR 50
 
@@ -7,4 +7,4 @@ char** read_dictionary(int* cnt);
 char* find_string(char** dict, int language, int complexity, int cnt);
 char* user_string(double* time, char* spec_string);
 
-#endif // _KEYBOARDNINJA_
+#endif // _KBN_READ_
