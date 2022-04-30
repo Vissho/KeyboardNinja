f6c4ec75aeeb0c218b001f1e33f381d0c065b4cc
Ilya Lisovitskyi <Vissho@users.noreply.github.com>
2022-04-30 22:29:56 +0700

    Fixed an error where the wrong analysis of Russian symbols is going on

--- src/keyboardninja/main.c
+++ src/keyboardninja/main.c
@@ -8,15 +8,29 @@ int main()
     double time;
     char** dict = read_dictionary(&cnt);
     printf("keyboardninja\n");
-    char* spec_string = find_string(dict, 2, 1, cnt);
+    char* spec_string = find_string(dict, 1, 1, cnt);
     char* user_str = user_string(&time, spec_string);
     printf("%s - %.2lf\n", user_str, time);
     int* analyz_print = analyz(spec_string, user_str, &cnt);
     for(int i = 0; i < cnt; i++){
         if(analyz_print[i] == 1){
-            printf("\033[32m%c", user_str[i]);
+            if(user_str[i] == -48 || user_str[i] == -47){
+                printf("\033[32m");
+                printf("%c", user_str[i]);
+                i++;
+                printf("%c", user_str[i]);
+            }else{
+                printf("\033[32m%c", user_str[i]);
+            }
         }else{
-            printf("\033[31m%c", user_str[i]);
+            if(user_str[i] == -48 || user_str[i] == -47){
+                printf("\033[31m");
+                printf("%c", user_str[i]);
+                i++;
+                printf("%c", user_str[i]);
+            }else{
+                printf("\033[31m%c", user_str[i]);
+            }
         }
     }
     printf("\n");
--- src/libkeyboardninja/kbn_analyz.c
+++ src/libkeyboardninja/kbn_analyz.c
@@ -12,10 +12,22 @@ int* analyz(char* spec_string, char* user_string, int* size)
     int* analyz = (int*)malloc(cnt * sizeof(int));
     i = 0;
     while(spec_string[i] != '\n'){
-        if(user_string[i] == spec_string[i]){
-            analyz[i] = 1;
+        if(user_string[i] == -48 || user_string[i] == -47){
+            if(user_string[i] == spec_string[i] && user_string[i+1] == spec_string[i+1]){
+                analyz[i] = 1;
+                i++;
+                analyz[i] = 1;
+            }else{
+                analyz[i] = 0;
+                i++;
+                analyz[i] = 0;
+            }
         }else{
-            analyz[i] = 0;
+            if(user_string[i] == spec_string[i]){
+                analyz[i] = 1;
+            }else{
+                analyz[i] = 0;
+            }
         }
         i++;
     }
@@ -23,6 +35,6 @@ int* analyz(char* spec_string, char* user_string, int* size)
         analyz[i] = 0;
         i++;
     }
-    *size = i - 1;
+    *size = cnt - 1;
     return analyz;
 }
