780ed80be091f881f0d999e7c9266091cb794433
Ilya Lisovitskyi <Vissho@users.noreply.github.com>
2022-05-19 13:56:42 +0700

    Created a test for reading the dictionary

--- test/ctest.c
+++ test/ctest.c
@@ -6,3 +6,15 @@
 #include <libkeyboardninja/kbn_analyz.h>
 #include <libkeyboardninja/kbn_read.h>
 
+CTEST(read_the_dictionary, correctly_reading_the_dictionary)
+{
+    char** dictionary = read_dictionary(SIZE_DICTIONARY);
+
+    for (int i = 0; i < SIZE_DICTIONARY; i++)
+        ASSERT_NOT_NULL(dictionary[i]);
+    ASSERT_NOT_NULL(dictionary);
+
+    for (int i = 0; i < SIZE_DICTIONARY; i++)
+        free(dictionary[i]);
+    free(dictionary);
+}
