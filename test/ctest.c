81dee3171caed8aa1e618b1b0594da27d462bc6d
Ilya Lisovitskyi <Vissho@users.noreply.github.com>
2022-05-19 14:14:30 +0700

    Created a test to search for a specified string

--- test/ctest.c
+++ test/ctest.c
@@ -18,3 +18,20 @@ CTEST(read_the_dictionary, correctly_reading_the_dictionary)
         free(dictionary[i]);
     free(dictionary);
 }
+
+CTEST(search_a_specified_string, correctly_search_a_specified_string)
+{
+    char** dictionary = read_dictionary(SIZE_DICTIONARY);
+    char* spec_string;
+    const int max_language = 2, max_complexity = 3;
+    for (int i = 1; i <= max_language; i++) {
+        for (int j = 1; j <= max_complexity; j++) {
+            spec_string = find_string(dictionary, i, j, SIZE_DICTIONARY);
+            ASSERT_NOT_NULL(spec_string);
+        }
+    }
+
+    for (int i = 0; i < SIZE_DICTIONARY; i++)
+        free(dictionary[i]);
+    free(dictionary);
+}
